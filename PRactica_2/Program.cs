d2fb4aa9a51614626eec59c0d5d8882693dec220
pablez <pablogr2928@gmail.com>
2025-05-10 22:18:05 -0400

    curso de C#

---
+++ PRactica_2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PRactica_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usando WriteLine
+            Console.WriteLine("Hola");
+            Console.WriteLine("Mundo");
+
+            Console.WriteLine("--- Separador ---"); // Para ver la diferencia
+
+            // Usando Write
+            Console.Write("Adiós "); // Nota el espacio al final de "Adiós "
+            Console.Write("Mundo ");
+            Console.Write("Cruel");
+
+            Console.WriteLine(); // Agrego un WriteLine vacío para bajar a la siguiente línea antes de que termine el programa
+            Console.WriteLine("--- Fin ---");
+
+            Console.ReadKey(); // Espera a que el usuario presione una tecla antes de cerrar la consola
+        }
+    }
+}
