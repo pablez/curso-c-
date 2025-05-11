bc21ecc9e1485e9edda3827a95905cfa81955634
pablez <pablogr2928@gmail.com>
2025-05-10 22:38:01 -0400

    segundo commit

--- PRactica_2/Program.cs
+++ PRactica_2/Program.cs
@@ -6,19 +6,8 @@
         {
             // Usando WriteLine
             Console.WriteLine("Hola");
-            Console.WriteLine("Mundo");
-
-            Console.WriteLine("--- Separador ---"); // Para ver la diferencia
-
-            // Usando Write
-            Console.Write("Adiós "); // Nota el espacio al final de "Adiós "
-            Console.Write("Mundo ");
-            Console.Write("Cruel");
-
-            Console.WriteLine(); // Agrego un WriteLine vacío para bajar a la siguiente línea antes de que termine el programa
-            Console.WriteLine("--- Fin ---");
-
-            Console.ReadKey(); // Espera a que el usuario presione una tecla antes de cerrar la consola
+            
+            Console.ReadKey(); 
         }
     }
 }
